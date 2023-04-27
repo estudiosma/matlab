f22d3350a5e8a5643d14cd617da7b2c9c4bf29cf
Estudios MA <89094693+estudiosma@users.noreply.github.com>
2023-04-27 11:47:12 -0500

    Update ma_fft.m

--- ma_functions/ma_fft.m
+++ ma_functions/ma_fft.m
@@ -2,18 +2,19 @@
 % Version 1.0
 % Date: 27-abr-2022
 % Autor: Molina Vidal D.A. by Estudios MA
-% Github: https://github.com/estudiosma/matlab
+% Download more functions on our Github:
+% https://github.com/estudiosma/matlab
 %
 % [f, magnitude] = fft_ma(data, fs)
 % fft Discrete Fourier transform.
 %
 % Example:
-% data = ;
-% fs = ;
+% data = ;  % 'data' is a vector Nx1
+% fs = ;    % sampling frequency
 % [f, magnitude] = ma_fft(data, fs);
 % figure;
-% plot(f, magnitude); grid minor
-% xlabel('Freq [Hz]');   ylabel('Amp');
+% plot(f, magnitude);   grid minor
+% xlabel('Freq [Hz]');  ylabel('Amp');
 %
 function [f, magnitude] = ma_fft(data, fs)
 fprintf("Download more functions: https://github.com/estudiosma/matlab");
