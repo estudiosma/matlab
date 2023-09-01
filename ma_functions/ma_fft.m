9394814a78e1966c3569cbf614910bcf971e61ff
Estudios MA <89094693+estudiosma@users.noreply.github.com>
2023-09-01 20:43:46 -0300

    Update ma_fft.m
    Corrige el tamaña de la matriz de datos de entrada.

--- ma_functions/ma_fft.m
+++ ma_functions/ma_fft.m
@@ -1,16 +1,17 @@
 % 
-% Version 1.0
-% Date: 27-abr-2022
+% Version 1.1
+% Date: 31-ago-2023
 % Autor: Molina Vidal D.A. by Estudios MA
 % Contact: estudiosma01@gmail.com
 % Download more functions on our Github:
 % https://github.com/estudiosma/matlab
 %
 % [f, magnitude] = fft_ma(data, fs)
-% fft Discrete Fourier transform.
+% fft_ma is the magnitude of the discrete Fourier transform (DFT) of 
+% 'data'.
 %
 % Example:
-% data = ;  % 'data' is a vector Nx1
+% data = ;  % 'data' can be an Nx1 vector or matrix
 % fs = ;    % sampling frequency
 % [f, magnitude] = ma_fft(data, fs);
 % figure;
@@ -18,9 +19,15 @@
 % xlabel('Freq [Hz]');  ylabel('Amp');
 %
 function [f, magnitude] = ma_fft(data, fs)
-fprintf("Download more functions: https://github.com/estudiosma/matlab");
+
+[row, col] = size(data);
+
+if (col>row)
+    data = data';
+end
+
 NNFT = length(data);   tamano = length(data);
 %----- FFT
 Y=fft(data,NNFT)/tamano;
 f = fs/2*linspace(0,1,NNFT/2);
-magnitude = abs(Y(1:NNFT/2));
+magnitude = 2*abs(Y(1:NNFT/2, :));
