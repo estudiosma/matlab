7201884adc1e1399a05af859bb3014feed9c31ec
Estudios MA <89094693+estudiosma@users.noreply.github.com>
2023-09-01 22:31:24 -0300

    Update ma_fft_plot.m
    Corrige la dimensión de la matriz., en caso no sea la correcta.

--- ma_functions/ma_fft_plot.m
+++ ma_functions/ma_fft_plot.m
@@ -1,29 +1,43 @@
 % 
-% Version 1.0
-% Date: 27-abr-2023
-% Autor: Molina Vidal D.A. by Estudios MA
-% Contact: estudiosma01@gmail.com
+% Version:  1.3
+% Date:     31-ago-2023
+% Autor:    Molina Vidal D.A. by Estudios MA
+% Contact:  estudiosma01@gmail.com
 % Download more functions on our Github:
 % https://github.com/estudiosma/matlab
 %
 % ma_fft_plot(data, fs, new_figure)
-% Plot fft Discrete Fourier transform.
+% Plot in a new figure the magnitude of the discrete Fourier transform 
+% (DFT) of 'data'.
 %
 % Example:
-% data = ;  % 'data' is a vector Nx1
+% data = ;  % 'data' can be an Nx1 vector or matrix
 % fs = ;    % sampling frequency
-% new_figure = 1;   % 1 to plot on a new figure Any other value to plot 
-% on an open figure.
-% ma_fft_plot(data, fs, 1);
+% new_figure = 1;   % 1 to plot on a new figure (this is a default value). 
+% Any other value to plot on an open figure.
+% ma_fft_plot(data, fs, new_figure);
 %
 function ma_fft_plot(data, fs, new_figure)
+% default value
+if  nargin < 3
+    new_figure = 1;
+end
+
+[row, col] = size(data);
+
+if (col>row)
+    data = data';
+end
+
 NNFT = length(data);   tamano = length(data);
 %----- FFT
 Y=fft(data,NNFT)/tamano;
 f = fs/2*linspace(0,1,NNFT/2);
-magnitude = abs(Y(1:NNFT/2));
+magnitude = 2*abs(Y(1:NNFT/2, :));
+
 if new_figure == 1
     figure;
 end
+
 plot(f, magnitude); grid minor
 xlabel('Freq [Hz]');   ylabel('Amp');
