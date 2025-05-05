f4cfa4cff4e5ddb68fc5cd41851114ae4607b6eb
Danilo <estudiosma01@gmail.com>
2025-05-05 10:55:36 -0300

    Add example

--- ma_functions/example.m
+++ ma_functions/example.m
@@ -5,7 +5,7 @@ clc;
 % Create a sample sine wave
 fs = 1000;                 % Sampling frequency (Hz)
 t = 0:1/fs:1-1/fs;         % Time vector
-signal = sin(2*pi*50*t);   % 50 Hz sine wave
+signal = sin(2*pi*60*t);   % 50 Hz sine wave
 
 % Compute and plot FFT
 ma_fft_plot(signal, fs);
