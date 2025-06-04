302c7efbb813954216de9e231894c692fd935e5b
Danilo Molina <estudiosma01@gmail.com>
2025-06-04 20:35:21 -0300

    Update units FFT

--- ma_functions/ma_fft_plot.m
+++ ma_functions/ma_fft_plot.m
@@ -66,7 +66,7 @@ if showPlot
     end
     plot(f, magnitude);
     xlabel('Frequency (Hz)');
-    ylabel('Magnitude');
+    ylabel('Amplitude (V)');
     grid on;
     title('FFT Magnitude (Amplitude) Spectrum');
 end
